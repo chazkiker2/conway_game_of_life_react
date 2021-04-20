3695234829895328ae51a843288167a3bbb54c36
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-20 18:49:11 -0400

    refactor Header into a simpler, less intrusive design

--- src/features/common/index.js
+++ src/features/common/index.js
@@ -3,5 +3,5 @@ export { default as Link } from "./link";
 export { default as Button } from "./button";
 export { default as Loader } from "./loader";
 export { default as Heading } from "./heading";
-export { default as Header } from "./header/header";
+export { default as Header } from "./header";
 export { default as Wrapper } from "./wrapper";
