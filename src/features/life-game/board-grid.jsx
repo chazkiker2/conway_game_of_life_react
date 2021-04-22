86a23bfb266dcb3d3b522d67bff4afbac9a8c2d5
chaz-kiker <chaz-kiker@lambdastudents.com>
2021-04-22 15:34:33 -0400

    clean up board-grid.jsx and add documenting comments. delete old life class

--- src/features/life-game/board-grid.jsx
+++ src/features/life-game/board-grid.jsx
@@ -3,173 +3,251 @@ import seeds, { seedFrom, matrixFrom } from "../life/seeds"
 import Styled from "./board-grid.styles"
 import { Button } from "../common"
 
+/**
+ * Modulo operator that follows Python's functionality rather than JS
+ * This will be used to organize edge-overflow for cellular neighbors
+ * on the Life Board
+ * 
+ * JS:        -1 % 5 = -1
+ * Python:    -1 % 5 = 4
+ * mod:       mod(-1, 4) = 4
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @returns the remainder of a divided by b, in accordance with the Euclidean definition
+ */
 function mod(a, b) {
   return ((a % b) + b) % b
 }
 
-function arr_eq(a, b) {
-  if (a === b) {
-    return true;
-  }
-  return a.length === b.length &&
-    a.every((v, i) => v.every((v2, i2) => v2 === b[i][i2]))
-}
-
+/** a map of (key=seed.name: val=seed) pairs for ease of access to each seed by its name */
 const seedMap = Object.fromEntries(seeds.public.map(seed => [seed.name, seed]))
 
 
 const BoardGrid = props => {
+  // --------------------------------------
   // COMPONENT STATE
+  // --------------------------------------
+
+  /** the current generation of cells */
   const [current, setCurrent] = React.useState(null)
-  const [dim, setDim] = React.useState({ num_rows: 0, num_cols: 0 })
-  const { num_rows, num_cols } = dim;
-  const [playing, setPlaying] = React.useState(false)
-  const [clear, setClear] = React.useState(false)
+
+  /** the current dimension-size of our life grid */
+  const [dim, setDim] = React.useState({ numRows: 0, numCols: 0 })
+  const { numRows, numCols } = dim;
+
+  /** the name of the selected seed */
   const [seed, setSeed] = React.useState(seeds.default.name)
+
+  /** the rate at which to swap between generations (in milliseconds) */
   const [speed, setSpeed] = React.useState(1000) // default to 1 second
-  const [isResizing, setIsResizing] = React.useState(false);
-  const stopPlaying = () => setPlaying(false)
-  const startPlaying = () => setPlaying(true)
 
+  /** true if our simulation is actively running */
+  const [isRunning, setIsRunning] = React.useState(false)
+  const stopPlaying = () => setIsRunning(false)
+  const startPlaying = () => setIsRunning(true)
+
+  /** true if our user has triggered a "clear" */
+  const [isClearing, setIsClearing] = React.useState(false)
+
+  /** true if our user has triggered a resize of rows or columns */
+  const [isResizing, setIsResizing] = React.useState(false)
+
+  // --------------------------------------
   // EFFECTS
-  React.useEffect(() => {
-    const { dict, num_rows, num_cols } = seedMap[seed]
-    setDim({ num_rows, num_cols })
-    setCurrent({ ...dict })
-    return () => setClear(false)
-  }, [clear, seed])
+  // --------------------------------------
 
+  /** snags data from selected seed, sets relevant slices of state */
+  React.useEffect(() => {
+    const seedData = seedMap[seed]
+    setDim({ numRows: seedData.numRows, numCols: seedData.numCols })
+    setCurrent({ ...seedData.dict })
+    return () => setIsClearing(false)
+    // dep @isClearing -- b/c we'll reset to seed's data if user "clear"s
+    // dep @seed --  selected seed. set state any time seed is changed
+  }, [isClearing, seed])
+
+  /** attempts to preserve as much data possible while resizing the column & rows of our grid */
   React.useEffect(() => {
     if (
       !isResizing
       || !current
       || Object.entries(current).length === 0
     ) {
-      return
+      // this check prevents this effect from running on initial mount
+      // and any time `current` changes without the user triggering a resize
+      return // exit effect
     }
 
     const matrix = matrixFrom({ ...current });
-    const copy_matrix = matrix.map(sub => [...sub])
-    const diff_row = num_rows - matrix.length
-    const diff_col = num_cols - matrix[0].length
+    const copyMatrix = matrix.map(sub => [...sub])
+    const diffRow = numRows - matrix.length
+    const diffCol = numCols - matrix[0].length
 
-    if (diff_row === 0 && diff_col === 0) {
-      return
+    if (diffRow === 0 && diffCol === 0) {
+      // if there isn't a difference in size,
+      // then we most certainly shouldn't resize
+      return // exit effect
     }
 
-    for (let i = 0; i < Math.abs(diff_row); i++) {
-      if (diff_row > 0) {
-        copy_matrix.push(new Array(parseInt(num_cols)).fill(0))
+    for (let i = 0; i < Math.abs(diffRow); i++) {
+      if (diffRow > 0) {
+        copyMatrix.push(new Array(parseInt(numCols)).fill(0))
       } else {
-        copy_matrix.pop()
+        copyMatrix.pop()
       }
     }
 
-    for (let i = 0; i < copy_matrix.length; i++) {
-      for (let j = 0; j < Math.abs(diff_col); j++) {
-        if (diff_col > 0) {
-          copy_matrix[i].push(0);
+    for (let i = 0; i < copyMatrix.length; i++) {
+      for (let j = 0; j < Math.abs(diffCol); j++) {
+        if (diffCol > 0) {
+          copyMatrix[i].push(0);
         } else {
-          copy_matrix[i].pop()
+          copyMatrix[i].pop()
         }
       }
     }
 
-    const { dict } = seedFrom(copy_matrix.map(sub => [...sub]))
+    const { dict } = seedFrom(copyMatrix.map(sub => [...sub]))
     setCurrent(dict)
-    debugger;
 
     return () => {
       setIsResizing(false)
     }
-  }, [num_rows, num_cols, current, isResizing])
+  }, [numRows, numCols, current, isResizing])
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const generateMemoized = React.useCallback(generateNext, [current])
 
+  /** memoized b/c generateNext will always return the same result given the same argument */
+  const generateMemoized = React.useCallback(generateNext,
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [current])
+
+  /** simulates life if the user has clicked "play" button */
   React.useEffect(() => {
-    if (!playing) {
-      return
+    if (!isRunning) {
+      // if simulation is not running
+      return // exit effect
     }
+    // step through each generation at the given `speed` (ms)
     const timer = setInterval(() => {
       setCurrent({ ...generateMemoized() })
     }, speed)
-    return () => clearInterval(timer);
-  }, [playing, generateMemoized, speed])
+    // cleanup timer
+    return () => clearInterval(timer)
+    // dep @isRunning -- we want to run if isRunning changes
+    // dep @generateMemoized -- the memoized `generateNext` fn
+    // dep @speed -- the pace (in ms) at which we'd like to swap generations
+  }, [isRunning, generateMemoized, speed])
 
+
+  // --------------------------------------
   // UTILITIES
+  // --------------------------------------
+
+  /** given row and column coordinates, return the dictionary key format */
   const key = (i, j) => `${i}.${j}`
+  /** given a key representation of a row, column location, return coordinate pair */
   const unKey = k => k.split(".")
+  /** essentially a NOT operator for our 1 and 0 */
   const flip = entry => entry === 0 ? 1 : 0
 
+  // --------------------------------------
   // HANDLERS
+  // --------------------------------------
+
+  /** toggle cell state at the given coordinates */
   function handleCellClick(i, j) {
     const k = key(i, j)
     setCurrent(prev => ({ ...prev, [k]: flip(prev[k]) }))
   }
-
+  /** set the selected seed */
   function selectSeed(evt) {
-    const { value } = evt.target;
-    setSeed(value)
+    setSeed(evt.target.value)
   }
-
+  /** updates dimension state and triggers the resize effect */
   function handleResize(evt) {
     const { name, value } = evt.target;
     setDim(prev => ({ ...prev, [name]: value }))
-    setIsResizing(true);
+    setIsResizing(true)
   }
 
-  // CORE FUNCTIONALITY
+  // --------------------------------------
+  // CORE FUNCTIONALITY FOR "Game of Life"
+  // --------------------------------------
 
+
+  /**
+   * Finds all neighbor coordinates for the given cell.
+   * If the cell resides on the edge of the board, their neighbors wrap 
+   * to the other side of the board.
+   * 
+   * @param {int} i Index of sub-array where the relevant cell lives
+   * @param {int} j Index of cell for which to find neighbors
+   * @returns List of neighbor coordinates; edges of board wrap infinitely
+   */
   function getNeighborCoords(i, j) {
+    // coordinate pairs that have not yet been modulo-ed to wrap edge
     const noMod = [
-      [i - 1, j - 1],
-      [i - 1, j],
-      [i - 1, j + 1],
-      [i, j + 1],
-      [i + 1, j + 1],
-      [i + 1, j],
-      [i + 1, j - 1],
-      [i, j - 1],
+      [i - 1, j - 1], // up left
+      [i - 1, j],     // up 
+      [i - 1, j + 1], // up right
+      [i, j + 1],     // right
+      [i + 1, j + 1], // down right
+      [i + 1, j],     // down
+      [i + 1, j - 1], // down left
+      [i, j - 1],     // left
     ]
-    return noMod.map(([ni, nj]) => [mod(ni, dim.num_rows), mod(nj, dim.num_cols)])
+    // map through the neighbor coords and modulo each to wrap around the edge
+    return noMod.map(([ni, nj]) => [mod(ni, dim.numRows), mod(nj, dim.numCols)])
   }
 
+  /** counts the number of neighbor cells that are living for the given cell */
   function countLiveNeighbors(i, j) {
     return getNeighborCoords(i, j)
       .filter(([ni, nj]) => current[key(ni, nj)] !== 0)
-      .length;
+      .length
   }
 
+  /** generates the next layer of cells. */
   function generateNext() {
-    const copy = { ...current }
-    for (let i = 0; i < dim.num_rows; i++) {
-      for (let j = 0; j < dim.num_cols; j++) {
+    // make a copy that does not reference the same place in memory
+    const next = { ...current }
+    // for each cell in the grid, apply the rules of life
+    for (let i = 0; i < dim.numRows; i++) {
+      for (let j = 0; j < dim.numCols; j++) {
         const k = key(i, j)
         const isAlive = current[k] !== 0
         const numLiveNeighbors = countLiveNeighbors(i, j)
         const hasTwo = numLiveNeighbors === 2
         const hasThree = numLiveNeighbors === 3
-        copy[k] = (isAlive ? (hasTwo || hasThree) : hasThree) ? 1 : 0
+        // if the cell is alive and has 2-3 live neighbors, sustain life
+        // if the cell is dead and has 3 live neighbors, become alive
+        // otherwise, die or stay dead
+        next[k] = (isAlive ? (hasTwo || hasThree) : hasThree) ? 1 : 0
       }
     }
-    return copy
+    // return the next generation
+    return next
   }
 
+  /** swap our current generation with the next */
   function step() {
     setCurrent({ ...generateMemoized() })
   }
 
+  // --------------------------------------
+  // Render the following JSX
+  // --------------------------------------
   return (
     <>
       <Styled.Page>
         <Styled.Board>
           {
             current &&
-            <Styled.Grid num_col={dim.num_cols} num_rows={dim.num_rows}>
+            <Styled.Grid num_col={dim.numCols} num_rows={dim.numRows}>
               {
                 Object.entries(current)?.map(([k, value]) => {
-                  const [i, j] = unKey(k);
+                  const [i, j] = unKey(k)
                   return (
                     <Styled.Cell
                       key={k}
@@ -186,12 +264,12 @@ const BoardGrid = props => {
         <Styled.Toolbar>
           <div className="toolbar--one">
             {
-              playing
+              isRunning
                 ? <Button onClick={stopPlaying}>Stop</Button>
                 : <Button onClick={startPlaying}>Play</Button>
             }
             <Button onClick={step}>Step</Button>
-            <Button onClick={() => setClear(true)}>Clear</Button>
+            <Button onClick={() => setIsClearing(true)}>Clear</Button>
           </div>
           <div className="toolbar--two">
             <div>
@@ -208,11 +286,11 @@ const BoardGrid = props => {
             </div>
             <div>
               <label htmlFor="num_rows">Rows:</label>
-              <input type="number" name="num_rows" value={dim.num_rows} onChange={handleResize} />
+              <input type="number" name="num_rows" value={dim.numRows} onChange={handleResize} />
             </div>
             <div>
               <label htmlFor="num_cols">Columns:</label>
-              <input type="number" name="num_cols" value={dim.num_cols} onChange={handleResize} />
+              <input type="number" name="num_cols" value={dim.numCols} onChange={handleResize} />
             </div>
           </div>
         </Styled.Toolbar>
@@ -222,4 +300,4 @@ const BoardGrid = props => {
 };
 
 
-export default BoardGrid;
+export default BoardGrid
--- src/features/life/seeds.js
+++ src/features/life/seeds.js
@@ -5,7 +5,6 @@ export function createEmpty(rows, columns) {
     out[i] = new Array(columns).fill(0)
   }
   return out;
-  // return new Array(rows).fill(new Array(columns).fill(0));
 }
 
 export function dictFrom(matrix) {
@@ -18,25 +17,25 @@ export function matrixFrom(dict) {
   const deKey = k => k.split(".").map(num => parseInt(num))
   const key = (i, j) => `${i}.${j}`
 
-  let max_i = -1;
-  let max_j = -1;
+  let maxI = -1;
+  let maxJ = -1;
 
   for (const k in dict) {
     const [i, j] = deKey(k);
-    max_i = Math.max(max_i, i)
-    max_j = Math.max(max_j, j)
-    if (isNaN(max_j) || isNaN(max_i)) console.log({ i, j, k, s: k.split(".").map(num => parseInt(num)) })
+    maxI = Math.max(maxI, i)
+    maxJ = Math.max(maxJ, j)
+    if (isNaN(maxJ) || isNaN(maxI)) console.log({ i, j, k, s: k.split(".").map(num => parseInt(num)) })
   }
 
-  if (max_i === -1) {
+  if (maxI === -1) {
     return []
-  } else if (max_j === -1) {
-    return new Array(max_i).fill(0)
+  } else if (maxJ === -1) {
+    return new Array(maxI).fill(0)
   }
 
-  const matrix = createEmpty(max_i + 1, max_j + 1)
-  for (let i = 0; i < max_i; i++) {
-    for (let j = 0; j < max_j; j++) {
+  const matrix = createEmpty(maxI + 1, maxJ + 1)
+  for (let i = 0; i < maxI; i++) {
+    for (let j = 0; j < maxJ; j++) {
       const val = dict[key(i, j)]
       matrix[i][j] = val
     }
@@ -122,7 +121,7 @@ pulsar.name = "Pulsar"
 
 
 // 18 rows x 11 col
-const i_column = seedFrom(
+const iColumn = seedFrom(
   [
     //, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], /* 0  */
@@ -145,7 +144,7 @@ const i_column = seedFrom(
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], /* 17 */
   ],
 )
-i_column.name = "I-Column"
+iColumn.name = "I-Column"
 
 const def = seedFrom(createEmpty(25, 25))
 def.name = "DEFAULT"
@@ -160,7 +159,7 @@ const seeds = {
     def,
     pulsar,
     toad,
-    i_column,
+    iColumn,
   ],
   default: def,
 }
